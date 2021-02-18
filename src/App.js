cddf15310f8957edb8d839af485c371c71c043bd
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-02-18 11:17:53 +0100

    3. Siintaxis JSX

--- src/App.js
+++ src/App.js
@@ -2,23 +2,43 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
+	let nombre = 'Jon'
+	let auth = true
+	let estaciones = ['Primavera', 'Verano', 'Otoño', 'Invierno']
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Editar <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<>
+			<div className='App'>
+				<header className='App-header'>
+					<img src={logo} className='App-logo' alt='logo' />
+					<label htmlFor='nombre'>Nombre</label>
+					<input type='text' id='nombre' />
+					<h1>{nombre}</h1>
+					<p>
+						{auth ? 'El usuario está logeado' : 'El usuario no está logeado'}
+					</p>
+					<p>{2 + 1}</p>
+					<p>
+						Editar <code>src/App.js</code> and save to reload.
+					</p>
+
+					<ul>
+						{estaciones.map((el, index) => (
+							<li key={index}>{el}</li>
+						))}
+					</ul>
+
+					<a
+						className='App-link'
+						href='https://reactjs.org'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						Learn React
+					</a>
+				</header>
+			</div>
+			<div></div>
+		</>
 	)
 }
 
