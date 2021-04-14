db5d5801e58154ef43e9edf8c34a9815da7524f8
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-04-14 16:48:58 +0200

    22. Estilos CSS

---
+++ src/components/Estilos.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Estilos.css'
+// import './Estilos.scss'
+import moduleStyles from './Estilos.module.css'
+
+export default function Estilos() {
+	let myStyles = {
+		borderRadious: '.5rem',
+		margin: '2rem auto',
+		maxWidth: '50%',
+	}
+	return (
+		<section className='estilos'>
+			<h2>Estilos CSS en React</h2>
+			<h3 className='bg-react'>Estilos en hoja CSS externa</h3>
+			<h3
+				className='bg-react'
+				style={{ borderRadius: '.25rem', margin: '1rem' }}
+			>
+				Estilos en línes
+			</h3>
+			<h3 className='bg-react' style={myStyles}>
+				Agregando normalize con
+				<br />
+				<code>@import-normalize;</code>
+			</h3>
+			<h3 className={moduleStyles.error}>Estilos con Módulos</h3>
+			<h3 className={moduleStyles.success}>Estilos con Módulos</h3>
+			{/*<h3 className='bg-sass'>Estilos con SASS</h3>*/}
+		</section>
+	)
+}
